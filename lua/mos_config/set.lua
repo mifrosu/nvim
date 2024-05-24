f5a632210c5233f478a4275708845fc3b67521ef
Michael O'Sullivan <michael.osullivan@f-secure.com>
2024-05-24 11:35:08 +0100

    Increase updatetime

--- lua/mos_config/set.lua
+++ lua/mos_config/set.lua
@@ -22,7 +22,7 @@ vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 
-vim.opt.updatetime = 50
+vim.opt.updatetime = 250
 
 -- set a colorcolumn at 80 characters
 -- vim.opt.colorcolumn = "80"
