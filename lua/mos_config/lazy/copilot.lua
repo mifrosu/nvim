fcc6c13343e54bae713ea0c10ed85399be65e306
Michael O'Sullivan <michael.osullivan@f-secure.com>
2024-06-11 15:13:07 +0100

    Add lsp updates

--- lua/mos_config/lazy/copilot.lua
+++ lua/mos_config/lazy/copilot.lua
@@ -1,5 +1,20 @@
 return {
-	{
-		"github/copilot.vim",
-	},
+    -- {
+        -- "github/copilot.vim",
+    -- },
+    {
+        "zbirenbaum/copilot-cmp",
+        event = "InsertEnter",
+        config = function () require("copilot_cmp").setup() end,
+        dependencies = {
+            "zbirenbaum/copilot.lua",
+            cmd = "Copilot",
+            config = function()
+                require("copilot").setup({
+                    suggestion = { enabled = false },
+                    panel = { enabled = false },
+                })
+            end,
+        },
+    },
 }
